897c168e1f9b5f3bfa04f75ad65a01522c68474f
Lans12345 <olanalans12345@gmail.com>
2024-02-20 16:53:33 +0800

    added multiple choice

--- lib/screens/pages/multiple_choice_dialog.dart
+++ lib/screens/pages/multiple_choice_dialog.dart
@@ -15,93 +15,163 @@ class MultipleChoicePage extends StatefulWidget {
 class _MultipleChoicePageState extends State<MultipleChoicePage> {
   int _currentQuestionIndex = 0;
   final List<String> _questions = [
-    'Which of the following are factors of 15?',
-    'All of the following are factors of 12. Which of these is not?',
-    'Which of the following is not a multiple of 4?',
-    'Choose the correct multiples of 3.',
-    'Which is a prime number?',
-    'Which of the following is a composite number?',
-    'Which of these numbers is a prime number?',
-    'Write 16 as a product of its prime factors.',
-    'How is 48 written as a product of its prime factors?',
-    'Write 72 as a product of its prime factor.',
-    'What is the Greatest Common Factor (GCF) of 14 and 28?',
-    'What is the Greatest Common Factor (GCF) of 12 and 18?',
-    'What are the common factors of 21 and 35?',
-    'What is the Greatest Common Multiple (LCM) of 8 and 3?',
-    'What is the Least Common Multiple (LCM) of 9 and 15?',
-    'What are the first 5 common multiples of 4 and 8?',
-    '''Jarcel exercise every 12 days and Jayvee every 8 day. Jarcel
-and Jayvee both exercised today. How many days will it be
-until they exercise together again?''',
     '''
-Three girls went swimming at the river. After swimming, they
-collected stones. They collected 48 white stones and 60
-brown stones. What was the greatest number of stones they
-collected per hour if they collected the same number of stones
-per hour?
+Choose the correct number represent by each of the following:
+100+20+1=_______
+
 ''',
     '''
-Dayne and Amielle are going to pack old books with 6 Science
-books and 8 mathematics books in a box. What will be the
-smallest number of Science books and Mathematics books
-that they can pack if these are of the same number?
+Choose the correct number represent by each of the following:
+4000+ 200+10+1=_______
+
 ''',
     '''
-Lyka baked 48 cookies and 64 cupcakes. If she will pack
-these in boxes with the same number, what is the biggest
-number of cookies and cupcakes that can be packed in a
-box?
+Choose the correct missing terms of the following number sequence.
+1, 2, 4, _, 11
+
 ''',
+    '''
+Choose the correct missing terms of the following number sequence.
+1, 3, 7, _, 31, 63
+
+''',
+    '''
+Choose the correct missing terms of the following number sequence.
+1, 2, 4, 7,  _
+
+''',
+    '''
+What is the Least Common Denominator (LCD) of the following fraction.
+2/3 and 4/6
+
+''',
+    '''
+What is the Least Common Denominator (LCD) of the following fraction.
+½ and ¼ 
+
+''',
+    '''
+What is the correct place value of the following underlined number.
+1 4 3 1
+
+    ''',
+    '''
+What is the correct place value of the following underlined number.
+4 3 1
+
+''',
+    '''
+ Choose the correct numbers of the following symbols. 
+FIFTY-FIVE THOUSAND SIXTEEN 
+
+''',
+    '''
+Choose the correct numbers of the following symbols. 
+ONE HUNDRED SEVENTY
+
+''',
+    '''
+Choose the correct factor of the given number 
+12
+
+''',
+    'What is the word form of 0.65?',
+    '''
+Which number correctly completes the questions?
+20 x 5 = _____
+
+''',
+    '''
+Which number correctly completes the questions?
+100 x 2 = _____
+
+''',
+    ' Dana used a rule to make a number pattern. Her rule is to multiply by 2. Which number pattern follows Dana’s rule? ',
+    'Bridget’s uncle was born in the year 1935.  What digit is in the hundreds place in 1935? ',
+    ' The highest city in the world is 12,087 feet above sea level. What is the place value of the 2 in 12,087? ',
+    'Wiley Post traveled fifteen thousand, five hundred ninety-six miles. Which shows that distance written as a numeral?  ',
+    ' What is five hundred two and seventy-one hundredths written in numerals? ',
+    'Which number has the greatest value?',
+    ' Which number is a multiple of 4?',
+    'Which sentence is true?',
+    'choose the correct QUOTIENT of the given numbers. 25 divided by 5',
+    ' choose the correct DIFFERENCE of the given numbers.',
+    'choose the correct DIFFERENCE of the given numbers.',
+    'choose the correct QUOTIENT of the given numbers.',
+    'choose the correct DIFFERENCE of the given numbers.',
+    'Choose the correct name of the Polygon that has 3 equal sides and 3 angles.',
   ];
   final List<List<String>> _options = [
-    ['1,3,5,7,13,15', '15,30', '1,3,5,15', '5,10,15'],
-    ['8', '3', '4', '6'],
-    ['16', '18', '36', '28'],
-    ['6,9,15,21,27', '3,6,8,12,18', '12,14,16,18,20', '9,16,21,24,30'],
-    ['99', '93', '97', '95'],
-    ['47', '41', '49', '43'],
-    ['35', '59', '51', '48'],
-    ['2 x 2 x 2 x 2 x 2', '2 x 3 x 3', '2 x 2 x 2 x 2', '2 x 2 x 3'],
-    ['2 x 3 x 8', '2 x 2 x 2 x 6', '2 x 2 x 2 x 2 x 3', '6 x 8'],
+    ['121', '123', '122', '124'],
+    ['2141', '4211', '2114', '2411'],
+    ['7', '5', '3', '2'],
+    ['8', '10', '9', '15'],
+    ['11', '5', '3', '2'],
+    ['2', '12', '6', '5'],
+    ['2', '8', '4', '12'],
+    ['Ones', 'Hundreds', 'Tens', 'Thousands'],
+    ['Ones', 'Hundreds', 'Tens', 'Thousands'],
+    ['55,016', '55,0016', '55,160', '55,00016'],
+    ['107', '170', '10070', '1700'],
+    ['2x2x2', '2x2x4', '2x2x3', '2x2x2x2'],
     [
-      '3 x 3 x 3 x 3',
-      '2 x 2 x 2 x 3',
-      '2 x 2 x 2 x 2 x 2 x 2',
-      '2 x 2 x 2 x 3 x 3'
+      'Sixty-five',
+      'Sixty-Five Hundredths',
+      'Sixty-Five Tenths',
+      'Sixty-Five Thousandths'
     ],
-    ['7', '9', '14', '28'],
-    ['9', '6', '3', '12'],
-    ['5 and 7', '3 and 7', '1 and 5', '1 and 7'],
-    ['8', '24', '48', '3'],
-    ['6', '45', '3', '15'],
-    ['4,8,12,16,24', '5,10,15,20,25', '8,16,24,32,40', '2,4,6,8,10'],
-    ['20', '18', '12', '24'],
-    ['12', '20', '6', '24'],
-    ['16', '12', '24', '32'],
-    ['24', '32', '64', '16'],
+    ['100', '50', '110', '40'],
+    ['200', '500', '100', '400'],
+    [
+      '4, 6, 9, 10, 12 ',
+      '2, 4, 8, 16, 32',
+      '5, 7, 9, 11, 13 ',
+      '1, 3, 6, 9, 12'
+    ],
+    ['1', '9', '3', '5'],
+    ['ten thousands  ', 'thousands', 'hundreds ', 'tens '],
+    ['1556   ', '1596', '15,596', '15,956'],
+    ['5027.10    ', '527.10 ', '502.71', '52.71 '],
+    ['0.45    ', '0.54', '4.05 ', '4.50'],
+    ['5', '6', '7', '8'],
+    ['10 > 5', '3 > 5', '5 < 2', '10 = 5'],
+    ['10', '3', '5', '2'],
+    ['24', '25', '50', '26'],
+    ['200', '100', '300', '1000'],
+    ['10', '30', '50', '20'],
+    ['50', '100', '75', '25'],
+    ['Quadrilateral', 'Rumbos', 'Triangle', 'Circle'],
   ];
   final List<int> _answers = [
+    0,
+    1,
+    0,
+    3,
+    0,
+    2,
     2,
     0,
     1,
     0,
-    2,
-    2,
+    1,
     1,
     2,
-    0,
-    3,
-    2,
     1,
+    0,
+    1,
+    1,
+    0,
+    1,
+    2,
     3,
     3,
+    0,
     2,
     0,
-    0,
-    0,
-    0,
-    3,
+    1,
+    2,
+    2,
+    2
   ]; // Index of the correct answer for each question
 
   @override
@@ -209,7 +279,7 @@ box?
       barrierColor: Colors.white,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Quiz Finished'),
+          title: const Text('Multiple Choice Finished'),
           content: Text(
               'You answered $score out of ${_questions.length} questions correctly.'),
           actions: <Widget>[

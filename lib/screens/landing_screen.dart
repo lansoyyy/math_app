e2b78415a392c3f9e08bc5c7cee4899c918cceeb
Lans12345 <olanalans12345@gmail.com>
2024-03-13 05:49:27 +0800

    changed bg image of landing page

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:mathalino/screens/home_screen.dart';
 import 'package:mathalino/widgets/button_widget.dart';
@@ -17,33 +18,32 @@ class LandingScreen extends StatelessWidget {
           color: Colors.white,
           image: DecorationImage(
               image: AssetImage(
-                'assets/images/back.png',
+                'assets/images/MathTALINO 1.jpg',
               ),
               opacity: 125,
               fit: BoxFit.fill),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            TextWidget(
-              text: 'MATHEMATICS LEARNER MATERIAL for\nGRADE 4',
-              fontSize: 48,
-              color: Colors.blue,
-              fontFamily: 'Bold',
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Card(
+                elevation: 50,
+                child: ButtonWidget(
+                  fontSize: 24,
+                  color: Colors.blue,
+                  label: 'START',
+                  onPressed: () {
+                    Get.off(() => const HomeScreen(),
+                        transition: Transition.circularReveal,
+                        duration: const Duration(seconds: 5));
+                  },
+                ),
+              ),
             ),
             const SizedBox(
-              height: 50,
-            ),
-            ButtonWidget(
-              fontSize: 24,
-              color: Colors.blue,
-              label: 'START',
-              onPressed: () {
-                Get.off(() => const HomeScreen(),
-                    transition: Transition.circularReveal,
-                    duration: const Duration(seconds: 5));
-              },
+              height: 100,
             ),
           ],
         ),
